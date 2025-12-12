bce116b9aec1d6a55d3e722eb9b31d7646870c6c
Abhishek-369V <abhishekmadanala@gmail.com>
2025-12-12 12:42:48 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
   return (
     <nav className="header flex  justify-between items-center bg-[#1a1a1a] text-white px-2 py-2 text-lg md:py-4 gap-2  md:px-8 sm:py-3 sm:px-4 ">
       <div className="webtitle flex items-center justify-center md:gap-1">
-        <img src='./public/Favicon.svg' alt='Logo' className='w-4 h-6 mr-2 md:w-6 '></img>
+        <img src='/Favicon.svg' alt='Logo' className='w-4 h-6 mr-2 md:w-6 '></img>
         <p className='logo text-xs font-bold opacity-90 md:text-xl sm:text-sm '>Text To Image Generator</p>
       </div>
       <ul className='flex gap-3 font-medium md:gap-8 sm:gap-5'>
